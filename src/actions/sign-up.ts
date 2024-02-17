631c53250dff147f1de945b9714cb957c0250af3
R1013-T <nosuke1013@icloud.com>
2024-02-17 13:09:15 +0900

    create sign up page

---
+++ src/actions/sign-up.ts
@@ -0,0 +1,26 @@
+'use server'
+
+import type { z } from 'zod'
+
+import { signUpSchema } from '@/lib/schemas'
+import type { ActionsResult } from '@/types/actions-result'
+
+export const signUp = async (
+  values: z.infer<typeof signUpSchema>,
+): Promise<ActionsResult> => {
+  const validatedFields = signUpSchema.safeParse(values)
+
+  if (!validatedFields.success) {
+    return {
+      isSuccess: false,
+      error: {
+        message: validatedFields.error.message,
+      },
+    }
+  }
+
+  return {
+    isSuccess: true,
+    message: 'サインアップに成功しました。',
+  }
+}
